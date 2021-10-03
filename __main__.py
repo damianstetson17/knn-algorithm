e845e019ed65934e026943ba12a5e18c6845fe54
Damian Stetson <damianstetson17@gmail.com>
2021-10-03 14:42:34 -0300

    reorganizated code for simplified

--- __main__.py
+++ __main__.py
@@ -1,96 +1,17 @@
 # pip install numpy
 # pip install matplotlib
-
-import time
+import gui
 from knn import Knn
 from data import Data
-from tkinter import *
-from tkinter import ttk
 from csv import reader
-import tkinter.messagebox
 from typing import Counter, List
 
-pathDataSetFile = "Not selected."
-KNumber = 1
-KFoldResults = []
-root = Tk()
-frame_values_selected = LabelFrame(root, text="SELECTED VALUES", padx=65,pady=71.5)
-
-#number k table label
-kfold_results_table = ttk.Treeview(frame_values_selected, columns=("#1","#2"), show="headings")
-kfold_results_table.heading("#1", text="K Value",anchor=N)
-kfold_results_table.heading("#2", text="Success",anchor=N)
-kfold_results_table.column("#1",anchor=N)
-kfold_results_table.column("#2",anchor=N)
-
-def GUI ():
-    global KNumber
-    global pathDataSetFile
-    
-    root.title("Main Menú - KNN - Stetson & Piotroski")
-    #initial values for start the algorithm
-    frame_values_selected.grid(row=1, column=1)
-    label_data_set_path = Label(frame_values_selected, text="DATA SET path file: "+pathDataSetFile)
-    label_data_set_path.pack()
-    #number k label
-    label_clusters = Label(frame_values_selected, text="Number of k selected: "+str(KNumber))
-    label_clusters.pack(pady=10)
-    
-    #start the K algorithm button
-    button_start_K = Button(frame_values_selected,text="START K ALGORITHM",
-                            command=startButton)
-    button_start_K.pack()
-
-    #inputs frame
-    frame_inputs = LabelFrame(root, text="INITIAL VALUES", padx=50,pady=50)
-    frame_inputs.grid(row=1, column=0)
-    button_select_data_set = Button(frame_inputs, text = "Select DATA SET .txt file", width=25,
-                                        command = lambda: setPathFile(label_data_set_path))
-    button_select_data_set.pack()
-
-    #k scale
-    k_scale_input = Scale(frame_inputs, label="Select the number of K:", 
-                            from_=1, to=10, orient=HORIZONTAL, length=200,
-                            command = lambda a: setKNumber(a,label_clusters))
-    k_scale_input.set(KNumber)
-    k_scale_input.pack(pady=20)
-
-    #KFoldResultlabel = Label(root, text="\n".join(map(str, yourlist)))
-    root.mainloop()
-
-"""START GUI BUTTONS FUNCIONS"""
-def setPathFile(label_data_set_path):
-    global pathDataSetFile
-    from tkinter import filedialog
-    pathDataSetFile = filedialog.askopenfilename(title="Select a data set",
-                                                filetypes = (("text files","*.txt"),("all files","*.*")))
-    label_data_set_path.config(text="DATA SET path file: "+pathDataSetFile)
-
-def setKNumber(val,label_clusters):
-        global KNumber
-        KNumber=val
-       #print(f"Number of clusters selected: {self.clustersNumber}")
-        label_clusters.config(text="Number of K selected: "+str(KNumber))
-
-def startButton():
-    if pathDataSetFile != "Not selected.":
-        if pathDataSetFile != "":
-            startKAlgorithm()
-        else:
-            tkinter.messagebox.showerror('ERROR AL INTENTAR EJECUTAR', 'Primero debe seleccionar un DATASET antes de ejecutar')
-    else:
-        tkinter.messagebox.showerror('ERROR AL INTENTAR EJECUTAR', 'Primero debe seleccionar un DATASET antes de ejecutar')
-"""END GUI BUTTONS FUNCIONS"""
-
 def startKAlgorithm():
-    global KNumber
-    global pathDataSetFile
-    global KFoldResults
+    #parse to int from GUI input
+    KNumber = int(gui.KNumber)
 
-    #parse to int from GUI
-    KNumber = int(KNumber)
-
-    with open(pathDataSetFile, 'r') as read_dataSet:
+    #read from .txt the dataset
+    with open(gui.pathDataSetFile, 'r') as read_dataSet:
         DataSet : List[Data] = []
         csv_reader = reader(read_dataSet)
 
@@ -104,27 +25,30 @@ def startKAlgorithm():
                     int(dataListRow[2])
                 )
             )
+    """
+    K FOLD VALIDATION EJEMPLO SENCILLO
+    a = (8,8,8,8,7,7,7,7)
+    aCounter = Counter(a)
+    EtiquetaMasRepetida = max(aCounter,key=aCounter.get)
+    ocurrenciasDeMaxValRep = 0
+    Kacierto=0
+    CantidadDeVecesDeLaEtiquetaMasRepetida = aCounter[EtiquetaMasRepetida]
+    for etiqueta in aCounter:
+        #si la cantidad de la etiqueta, es igual a la cantidad de la EtiquetaMasRepetida
+        if aCounter[etiqueta] == CantidadDeVecesDeLaEtiquetaMasRepetida:
+            ocurrenciasDeMaxValRep=ocurrenciasDeMaxValRep+1
+    #si la cantidad de la EtiquetaMasRepetida no se repite para otras etiquetas (no hay empate)
+    if ocurrenciasDeMaxValRep == 1:
+            Kacierto = Kacierto+1
+    print(f"el mas reptido de {a} es: {EtiquetaMasRepetida}, y por lo tanto se considera que hay: {Kacierto} acierto")
+    """
 
-    # a = (8,8,8,8,7,7,7,7)
-    # aCounter = Counter(a)
-    # EtiquetaMasRepetida = max(aCounter,key=aCounter.get)
-    # ocurrenciasDeMaxValRep = 0
-    # Kacierto=0
-    # CantidadDeVecesDeLaEtiquetaMasRepetida = aCounter[EtiquetaMasRepetida]
-    # for etiqueta in aCounter:
-    #     #si la cantidad de la etiqueta, es igual a la cantidad de la EtiquetaMasRepetida
-    #     if aCounter[etiqueta] == CantidadDeVecesDeLaEtiquetaMasRepetida:
-    #         ocurrenciasDeMaxValRep=ocurrenciasDeMaxValRep+1
-    # #si la cantidad de la EtiquetaMasRepetida no se repite para otras etiquetas (no hay empate)
-    # if ocurrenciasDeMaxValRep == 1:
-    #         Kacierto = Kacierto+1
-    # print(f"el mas reptido de {a} es: {EtiquetaMasRepetida}, y por lo tanto se considera que hay: {Kacierto} acierto")
-
-
-    #start algorithm
+    #Instace of a KNN object 
     knnAlgorithm = Knn(DataSet,KNumber)  
-    PuntosConEtiquetasDevecinos = []
 
+    """START K FOLD VALIDATION"""
+    KFoldResults = []
+    PuntosConEtiquetasDevecinos = []
     #cargamos la tabla en horizontal de cada punto con sus 9 vecinos
     for data in DataSet:
         PuntosConEtiquetasDevecinos.append(knnAlgorithm.findKFoldNeighbors(data)) 
@@ -155,28 +79,18 @@ def startKAlgorithm():
         KFoldResults.append((k,correctCount))
         correctCount = 0
     
-    print(f"la tabla al principio tiene: {len(kfold_results_table.get_children())}")
-    kfold_results_table.delete(*kfold_results_table.get_children())
-    for r in kfold_results_table.get_children():
-        root.kfold_results_table.delete(r)
-    root.update()
-    root.update_idletasks()
-    print(f"la tabla luego de .delete() tiene: {len(kfold_results_table.get_children())}")
+    #Cargamos la tabla con los resultados de Kfold
+    gui.kfold_results_table.delete(*gui.kfold_results_table.get_children())
     for Kinfo in KFoldResults:
-        #print(f"{Kinfo}\t")
-        #kfold_results_table.insert(parent="", index='end',iid=0, text="hola",values=(Kinfo))
-        kfold_results_table.insert("", 'end', values=Kinfo)
-    print(f"la tabla luego del for de Kinfo: {len(kfold_results_table.get_children())}")
-    kfold_results_table.pack(pady=10)
-    
-    #plot
+        gui.kfold_results_table.insert("", 'end', values=Kinfo)
+    gui.kfold_results_table.pack(pady=10)
+    """END K FOLD VALIDATION"""
+
+    #Execute KNN and generate the grid
     knnAlgorithm.GenerateGrid()
 
-    #show results popUp
-    #tkinter.messagebox.showinfo(title='K-Fold Validation:', message="\n".join(map(str, KFoldResults))) 
-
 def main ():
-    GUI()     
+    gui.generate_main_menu()     
 
 if __name__ == '__main__':
     main()
---
+++ gui.py
@@ -0,0 +1,75 @@
+from tkinter import *
+from tkinter import ttk
+import tkinter.messagebox
+import __main__
+
+pathDataSetFile = "Not selected."
+KNumber = 1
+root = Tk()
+frame_values_selected = LabelFrame(root, text="SELECTED VALUES", padx=65,pady=71.5)
+
+#number k table label
+kfold_results_table = ttk.Treeview(frame_values_selected, columns=("#1","#2"), show="headings")
+kfold_results_table.heading("#1", text="K Value",anchor=N)
+kfold_results_table.heading("#2", text="Success",anchor=N)
+kfold_results_table.column("#1",anchor=N)
+kfold_results_table.column("#2",anchor=N)
+
+def generate_main_menu ():
+    global KNumber
+    global pathDataSetFile
+    
+    root.title("Main Menú - KNN - Stetson & Piotroski")
+    #initial values for start the algorithm
+    frame_values_selected.grid(row=1, column=1)
+    label_data_set_path = Label(frame_values_selected, text="DATA SET path file: "+pathDataSetFile)
+    label_data_set_path.pack()
+    #number k label
+    label_clusters = Label(frame_values_selected, text="Number of k selected: "+str(KNumber))
+    label_clusters.pack(pady=10)
+    
+    #start the K algorithm button
+    button_start_K = Button(frame_values_selected,text="START K ALGORITHM",
+                            command=startButton)
+    button_start_K.pack()
+
+    #inputs frame
+    frame_inputs = LabelFrame(root, text="INITIAL VALUES", padx=50,pady=50)
+    frame_inputs.grid(row=1, column=0)
+    button_select_data_set = Button(frame_inputs, text = "Select DATA SET .txt file", width=25,
+                                        command = lambda: setPathFile(label_data_set_path))
+    button_select_data_set.pack()
+
+    #k scale
+    k_scale_input = Scale(frame_inputs, label="Select the number of K:", 
+                            from_=1, to=10, orient=HORIZONTAL, length=200,
+                            command = lambda a: setKNumber(a,label_clusters))
+    k_scale_input.set(KNumber)
+    k_scale_input.pack(pady=20)
+
+    #KFoldResultlabel = Label(root, text="\n".join(map(str, yourlist)))
+    root.mainloop()
+
+"""START GUI BUTTONS FUNCIONS"""
+def setPathFile(label_data_set_path):
+    global pathDataSetFile
+    from tkinter import filedialog
+    pathDataSetFile = filedialog.askopenfilename(title="Select a data set",
+                                                filetypes = (("text files","*.txt"),("all files","*.*")))
+    label_data_set_path.config(text="DATA SET path file: "+pathDataSetFile)
+
+def setKNumber(val,label_clusters):
+        global KNumber
+        KNumber=val
+       #print(f"Number of clusters selected: {self.clustersNumber}")
+        label_clusters.config(text="Number of K selected: "+str(KNumber))
+
+def startButton():
+    if pathDataSetFile != "Not selected.":
+        if pathDataSetFile != "":
+            __main__.startKAlgorithm()
+        else:
+            tkinter.messagebox.showerror('ERROR AL INTENTAR EJECUTAR', 'Primero debe seleccionar un DATASET antes de ejecutar')
+    else:
+        tkinter.messagebox.showerror('ERROR AL INTENTAR EJECUTAR', 'Primero debe seleccionar un DATASET antes de ejecutar')
+"""END GUI BUTTONS FUNCIONS"""
